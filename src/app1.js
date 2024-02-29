2e0ab3d8a923c4be9c773ade21fb37cec8ec195b
ziyi2 <18768107826@163.com>
2024-02-29 19:53:26 +0800

    feat: 新增 code-splitting  示例

--- src/app1.js
+++ src/app1.js
@@ -2,6 +2,7 @@ import { createApp, ref, h } from "vue";
 
 createApp({
   setup() {
+    // 详见 https://cn.vuejs.org/api/composition-api-setup.html#usage-with-render-functions
     const message = ref('Hello App 1');
     return () => h("div", message.value);
   },
